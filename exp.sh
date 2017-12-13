c1367d99a542ed476564a5cbc8511f0b6381466f
Li Qinbin <25541809+GODqinbin@users.noreply.github.com>
2017-12-12 18:46:49 -0800

    experiment

---
+++ exp.sh
@@ -0,0 +1,8 @@
+build/bin/thundersvm-train -c 100 -g 0.5 /home/qinbin/thundersvm_zeyi/thundersvm/dataset/a9a > log/a9a.log
+#build/bin/thundersvm-train -c 100 -g 0.125 /home/qinbin/thundersvm_zeyi/thundersvm/dataset/rcv1_train.binary > log/rcv1.log
+build/bin/thundersvm-train -c 4 -g 0.5 /home/qinbin/thundersvm_zeyi/thundersvm/dataset/real-sim > log/real-sim.log
+#build/bin/thundersvm-train -c 10 -g 0.5 /home/qinbin/thundersvm_zeyi/thundersvm/dataset/w8a > log/w8a.log
+#build/bin/thundersvm-train -c 10 -g 0.002 /home/qinbin/thundersvm_zeyi/thundersvm/dataset/cifar10.libsvm > log/cifar10.log
+#build/bin/thundersvm-train -c 1 -g 0.3 /home/qinbin/thundersvm_zeyi/thundersvm/dataset/connect-4 > log/connect-4.log
+build/bin/thundersvm-train -c 10 -g 0.125 /home/qinbin/thundersvm_zeyi/thundersvm/dataset/mnist.scale > log/mnist.log
+#build/bin/thundersvm-train -c 4 -g 0.5 /home/qinbin/thundersvm_zeyi/thundersvm/dataset/news20.scale > log/news20.scale.log
